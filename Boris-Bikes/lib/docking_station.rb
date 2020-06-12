08029d344b228f043fc6a7a2a9f8d5277c0afc76
Joanne Chen <joanne_r_chen@yahoo.com>
2020-06-12 22:13:12 +0100

    Step 15 finished

--- Boris-Bikes/lib/docking_station.rb
+++ Boris-Bikes/lib/docking_station.rb
@@ -1,15 +1,29 @@
 require_relative 'bike.rb'
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :bike_rack
+  def initialize
+    @bike_rack = []
+  end
 
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if empty?
+    @bike_rack
   end
-  
+
   def dock(bike)
-    @bike = bike
+    fail 'No space available to dock' if full?
+    @bike_rack << bike
+  end
+
+  private
+
+  def full?
+    @bike_rack.length >= 20
+  end
+
+  def empty?
+    @bike_rack.empty?
   end
 end
 
